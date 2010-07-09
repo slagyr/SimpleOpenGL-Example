8dc2d7beaba27ff25df4066140d6cc906c169e4a
Micah <micah@8thlight.com>
2010-07-08 22:26:05 -0500

    sweet rotation, perspective, lighting included.... it's a game now\!

--- simpleexample.c
+++ simpleexample.c
@@ -1,92 +1,133 @@
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <Glut/glut.h>
+#include <stdio.h>
 
 #define kWindowWidth 400
 #define kWindowHeight 300
 
 typedef GLfloat point[3]; 
 
-// point v[4] ={{0.0, 0.0, 1.0}, 
-//              {0.0, 0.942809, -0.333333}, 
-//              {-0.816497, -0.471405, -0.333333}, 
-//              {0.816497, -0.471405, -0.333333}};
-
-
 GLvoid DrawGLScene(void);
-// void tetrahedron(void);
-// void triangle(point a, point b, point c);
-void cube();
-void square(point a, point b, point c, point d);
+GLvoid ChangeSize(GLsizei w, GLsizei h);
+GLvoid RotateSome(void);
+GLvoid HandleKeyPress(int key, int x, int y);
+
+int lastFrameTime = 0;
+float xVelocity = 0;
+float yVelocity = 0;
+float xRotation = 45;
+float yRotation = 45;
 
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(kWindowWidth, kWindowHeight);
-  glutInitWindowPosition(100, 100);
-  glutCreateWindow ("simple opengl example");
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(kWindowWidth, kWindowHeight);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow ("simple opengl example");
 
-  glutDisplayFunc(DrawGLScene);
+	glutDisplayFunc(DrawGLScene);
+	glutReshapeFunc(ChangeSize);
+	glutIdleFunc(RotateSome);
+	glutSpecialFunc(HandleKeyPress);
 
-  glutMainLoop();
-  return 0;
+	printf("entering glutMainLoop()\n");
+	glutMainLoop();
+	return 0;
 }
 
 GLvoid DrawGLScene(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT); 
-  glColor3f(1.0,1.0,1.0); 
-  glLoadIdentity(); 
+	// printf("DrawGLScene\n");
+	glClear(GL_COLOR_BUFFER_BIT);
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glNormal3d(0, 0, 1);
+	GLfloat specular[] = {0, 0, 0, 1};
+	glLightfv(GL_LIGHT0, GL_POSITION,  specular);
+	GLfloat blue[] = {.1f, .1f, .8f, 1.f};
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, blue);
 
-  glRotatef(45, 0.5, 0.5, 0.5);
-
-  // tetrahedron(); 
-  cube();
-  glutSwapBuffers();
+	glTranslatef(0.0, 0.0, -2.0);
+	glRotatef(xRotation, 0.0, 1.0, 0.0);
+	glRotatef(yRotation, 1.0, 0.0, 0.0);
+	
+	glutSolidCube(1);
+	
+	glutSwapBuffers();
 }
 
-void cube()
+
+void ChangeSize(GLsizei w, GLsizei h)
 {
-	point ftl = {-0.5, 0.5, 0.5};
-	point ftr = {0.5, 0.5, 0.5};
-	point fbl = {-0.5, -0.5, 0.5};
-	point fbr = {0.5, -0.5, 0.5};
-	point btl = {-0.5, 0.5, -0.5};
-	point btr = {0.5, 0.5, -0.5};
-	point bbl = {-0.5, -0.5, -0.5};
-	point bbr = {0.5, -0.5, -0.5};
+	printf("ChangeSize(%d, %d)\n", w, h);
+	glViewport(0, 0, w, h);
 
-	square(ftl, ftr, fbr, fbl); //front
-	square(fbl, fbr, bbr, bbl); //bottom
-	square(btl, btr, bbr, bbl); // back
-	square(btl, btr, ftr, ftl); //top
-	square(ftr, btr, bbr, fbr); //right
-	square(ftl, fbl, bbl, btl); //left
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	gluPerspective(60.0f, w/h, 1, 10);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
-void square(point a, point b, point c, point d)
+void RotateSome(void)
 {
-  glBegin(GL_LINE_LOOP);
-  glVertex3fv(a); 
-  glVertex3fv(b); 
-  glVertex3fv(c); 
-  glVertex3fv(d);
-  glEnd();	
+	int time;
+	time = glutGet(GLUT_ELAPSED_TIME);
+	if(time - lastFrameTime < 20)
+		return;
+	else
+		lastFrameTime = time;
+		
+	xRotation += xVelocity;
+	if(xRotation >= 360)
+		xRotation -= 360;
+	else if(xRotation <= 0)
+		xRotation += 360;
+		
+	yRotation += yVelocity;
+	if(yRotation >= 360)
+		yRotation -= 360;
+	else if(yRotation <= 0)
+		yRotation += 360;
+		
+	glLoadIdentity();
+	glutPostRedisplay();
 }
 
-// void triangle( point a, point b, point c) 
-// { 
-//   glBegin(GL_LINE_LOOP);
-//   glVertex3fv(a); 
-//   glVertex3fv(b); 
-//   glVertex3fv(c); 
-//   glEnd(); 
-// }
-// 
-// void tetrahedron(void) 
-// { 
-//   triangle(v[0], v[1], v[2]); 
-//   triangle(v[3], v[2], v[1]); 
-//   triangle(v[0], v[2], v[3]); 
-// } 
+void HandleKeyPress(int key, int x, int y)
+{
+	switch(key)
+	{
+		case GLUT_KEY_UP : 
+			yVelocity += 0.1;
+			break;
+		case GLUT_KEY_DOWN : 
+			yVelocity -= 0.1;
+			break;
+		case GLUT_KEY_LEFT :
+			xVelocity -= 0.1;
+			break;
+		case GLUT_KEY_RIGHT :
+			xVelocity += 0.1;
+			break;
+	}
+	
+	if(xVelocity < -5)
+		xVelocity = -5;
+	else if(xVelocity > 5)
+		xVelocity = 5;
+		
+
+	if(yVelocity < -5)
+		yVelocity = -5;
+	else if(yVelocity > 5)
+		yVelocity = 5;
+		
+	printf("x: %f y: %f\n", xVelocity, yVelocity);
+}
